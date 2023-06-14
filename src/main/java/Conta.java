97ee85c1e124883dec84cd41d23bd6c484185553
flaviaadiniz <flaviaadiniz@gmail.com>
2023-06-13 21:02:13 -0300

    Adição de 2 novos atributos, construtor, getters & setters na classe Conta

--- src/main/java/Conta.java
+++ src/main/java/Conta.java
@@ -1,7 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Conta {
 
     private String agencia;
     private String banco;
     private String numeroConta;
+    private double saldo;
+    private List<Transacao> transacoes;
 
+    public Conta(String agencia, String banco, String numeroConta) {
+        this.agencia = agencia;
+        this.banco = banco;
+        this.numeroConta = numeroConta;
+        this.saldo = 0;
+        this.transacoes = new ArrayList<>();
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getBanco() {
+        return banco;
+    }
+
+    public void setBanco(String banco) {
+        this.banco = banco;
+    }
+
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(String numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public List<Transacao> getTransacoes() {
+        return transacoes;
+    }
+
+    public void setTransacoes(List<Transacao> transacoes) {
+        this.transacoes = transacoes;
+    }
 }
